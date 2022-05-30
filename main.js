5f7e17d68921e4a5632175aa3b9b98f30e762ee3
GreenDevald1523 <46032258+GreenDevald1523@users.noreply.github.com>
2022-05-30 20:06:02 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+    $('.sec-6-carousel').slick({
+        dots: false,
+        fadeSpeed: 1000,
+        arrows: false,
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // prevArrow: document.getElementById("arrow__left"),
+        // nextArrow: document.getElementById("arrow__right"),
+    })
+})
+
